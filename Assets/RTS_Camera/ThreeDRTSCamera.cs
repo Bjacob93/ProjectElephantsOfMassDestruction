149c0fa1d42e23658189203a2e49ede48909f1c9
Bjacob93 <Zeint@live.dk>
2016-09-22 11:08:24 +0200

    RTSCamera
    Uploaded the rts camera unity project files. We can ctake the files here
    and add to a camera in the final file to make the game, when needed

---
+++ Assets/RTS_Camera/ThreeDRTSCamera.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThreeDRTSCamera : MonoBehaviour {
+
+	public GameObject TopLeft, BotRight;
+
+	public float scrollZone = 30.0f;
+	public float scrollSpeed = 3.0f;
+
+	//camera zoom variables
+	public float cameraSpeed = 5.0f;
+	public float cameraZoomSpeed = 15.0f;
+	public float cameraZoomIn = 1.0f;
+	public float cameraZoomOut = 2.0f;
+
+	//camera orbit variables
+	public float xMax;
+	public float xMin;
+	public float yMax = 100.0f;
+	public float yMin = 2.0f;
+	public float zMax;
+	public float zMin;
+
+	//camera rotation variables
+	public float panSpeed = 0.2f;
+	public float panAngleMin = 20;
+	public float panAngleMax = 85;
+
+	private Vector3 desiredPosition;
+
+	// Use this for initialization
+	void Start () {
+		TopLeft = GameObject.Find ("GameMap/TheMap/BotRight");
+		BotRight = GameObject.Find ("GameMap/TheMap/TopLeft");
+
+		xMax = TopLeft.transform.position.x;
+		zMax = TopLeft.transform.position.z;
+
+		xMin = BotRight.transform.position.x;
+		zMin = BotRight.transform.position.z;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		//Camera movement
+
+		float x = 0, y = 0, z = 0;
+		float speed = scrollZone * 3 *  Time.deltaTime;
+		//WASD movement
+		if (Input.GetKey(KeyCode.W)){
+			z += speed;
+		}
+		if (Input.GetKey(KeyCode.A)){
+			x -= speed;
+		}
+		if (Input.GetKey(KeyCode.S)){
+			z -= speed;
+		}
+		if (Input.GetKey(KeyCode.D)){
+			x += speed;
+		}
+
+		//mouse movement
+		if (Input.mousePosition.x < scrollZone)
+			x -= speed;
+		else if (Input.mousePosition.x > Screen.width - scrollZone)
+			x += speed;
+
+		if (Input.mousePosition.y < scrollZone)
+			z -= speed;
+		else if (Input.mousePosition.y > Screen.height - scrollZone)
+			z += speed;
+
+		//zoom camera
+		y -= 40 * Input.GetAxis ("Mouse ScrollWheel");
+
+		Vector3 move = new Vector3(x, y, z) + desiredPosition;
+		move.x = Mathf.Clamp (move.x, xMin, xMax);
+		move.y = Mathf.Clamp (move.y, 10, yMax);
+		move.z = Mathf.Clamp (move.z, zMin, zMax);
+		desiredPosition = move;
+		transform.position = Vector3.Lerp(transform.position, desiredPosition, 0.2f);
+
+		//panning when zooming not working
+		var pan = GetComponent<Camera>().transform.eulerAngles.x - y * -1;
+		pan = Mathf.Clamp (pan, panAngleMin, panAngleMax);
+		if (y < 0 || GetComponent<Camera>().transform.position.y > (yMax / 4)){
+			GetComponent<Camera>().transform.eulerAngles = new Vector3(pan,0,0);
+		}
+
+
+	}
+}
