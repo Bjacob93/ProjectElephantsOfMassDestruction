cc53d23f1d00cae91c458d8d0f447a5beaba36e3
Runagar <runagar@gmail.com>
2016-10-05 10:56:51 +0200

    Working on Pathfinding

---
+++ Assets/Pathfinding/Astar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class Astar : MonoBehaviour {
+	public Vector3 targetPosition;
+
+	Seeker seeker;
+
+	void Start () {
+		seeker = GetComponent<Seeker> ();	
+
+		seeker.StartPath (transform.position, targetPosition, OnPathComplete);
+	}
+
+	void OnPathComplete(Path p){
+		Debug.Log ("Path Completed" + p.error);
+	}
+		
+
+	void Update () {
+	
+	}
+}
