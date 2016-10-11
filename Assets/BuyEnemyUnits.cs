d35ef6cf7769effc4727f3ca95189d5affafb158
Vincent Valentine <i.win-chri.s-tie@hotmail.com>
2016-10-11 16:13:59 +0200

    working enermy spawner

--- Assets/BuyEnemyUnits.cs
+++ Assets/BuyEnemyUnits.cs
@@ -1,47 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BuyEnemyUnits : MonoBehaviour {
+public class BuyEnemyUnits : MonoBehaviour
+{
 
-    public float restartDelay = 5f;         // Time to wait before restarting the level
+    public float restartDelay = 10f;         // Time to wait before restarting the level
     float restartTimer;                     // Timer to count up to restarting the level
 
     public GameObject enemyUnits;
     int enemymoney = 500;
 
-    void start() {
-   
-}
+    void start()
+    {
+        
+    }
 
-void update() {
+    void Update()
+    {
+        
+        Debug.Log("her");
         restartTimer += Time.deltaTime;
-
-        if (gameObject.tag == "EnemyBase")
+        ScoreManager sm = GameObject.FindObjectOfType<ScoreManager>();
+        if (enemymoney <= 0)
         {
-            ScoreManager sm = GameObject.FindObjectOfType<ScoreManager>();
-            if (enemymoney <=0)
-            {
-                Debug.Log("Not enought money");
-                return;
-            }
+            Debug.Log("Not enought money");
+            return;
+        }
 
+
+
+        if (restartTimer >= restartDelay)
+        {
             if (enemymoney > 0)
             {
-                Debug.Log("her");
-                if (restartTimer >= restartDelay)
-                {
-                    enemymoney -= GameObject.FindObjectOfType<AlliedMelee_AI_Health>().cost;
-                    Instantiate(enemyUnits, new Vector3(-45, 0, -45), Quaternion.Euler(0, 0, 0));
-                }
-               
+                enemymoney -= GameObject.FindObjectOfType<AlliedMelee_AI_Health>().cost;
+                Instantiate(enemyUnits, new Vector3(-45, 0, -45), Quaternion.Euler(0, 0, 0));
+                restartTimer = 0;
             }
 
-            //Debug.Log("fuck");
         }
+
+        //Debug.Log("fuck");
     }
 
     void OnMouseDown()
     {
-        
+
     }
 }
