e43c1144cda29c265e395124b4a0c714edf6bf17
Vincent Valentine <i.win-chri.s-tie@hotmail.com>
2016-10-11 15:57:11 +0200

    enermybase beta af en alpha

---
+++ Assets/BuyEnemyUnits.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuyEnemyUnits : MonoBehaviour {
+
+    public float restartDelay = 5f;         // Time to wait before restarting the level
+    float restartTimer;                     // Timer to count up to restarting the level
+
+    public GameObject enemyUnits;
+    int enemymoney = 500;
+
+    void start() {
+   
+}
+
+void update() {
+        restartTimer += Time.deltaTime;
+
+        if (gameObject.tag == "EnemyBase")
+        {
+            ScoreManager sm = GameObject.FindObjectOfType<ScoreManager>();
+            if (enemymoney <=0)
+            {
+                Debug.Log("Not enought money");
+                return;
+            }
+
+            if (enemymoney > 0)
+            {
+                Debug.Log("her");
+                if (restartTimer >= restartDelay)
+                {
+                    enemymoney -= GameObject.FindObjectOfType<AlliedMelee_AI_Health>().cost;
+                    Instantiate(enemyUnits, new Vector3(-45, 0, -45), Quaternion.Euler(0, 0, 0));
+                }
+               
+            }
+
+            //Debug.Log("fuck");
+        }
+    }
+
+    void OnMouseDown()
+    {
+        
+    }
+}
