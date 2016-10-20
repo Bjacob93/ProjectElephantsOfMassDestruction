9a47f93a528ce53fafc7610887cfbcd30702812e
Runagar <runagar@gmail.com>
2016-10-20 11:37:00 +0200

    Initialised List<> in CommandList and EditorList

--- Assets/Scripts/IGUIDragAndDrop/CommandList.cs
+++ Assets/Scripts/IGUIDragAndDrop/CommandList.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CommandList : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //Initialise lists for the Editor window. These will contain the Command objects that has been dragged to the Editor-window.
+    public List<Command> availableCommands = new List<Command>();
+    public List<Command> slots = new List<Command>();
+
+    public CommandDatabase commandDatabase;
+
+    void Start()
+    {
+        //Cache the database of commands so that we can always find any command we need.
+        commandDatabase = GameObject.FindGameObjectWithTag("CommandDatabase").GetComponent<CommandDatabase>();
+    }
 }
--- Assets/Scripts/IGUIDragAndDrop/EditorList.cs
+++ Assets/Scripts/IGUIDragAndDrop/EditorList.cs
@@ -5,8 +5,14 @@ using System.Collections.Generic;
 public class EditorList : MonoBehaviour {
 
     //Initialise lists for the Editor window. These will contain the Command objects that has been dragged to the Editor-window.
-    public List<Command> availableCommands = new List<Command>();
+    public List<Command> enteredCommands = new List<Command>();
     public List<Command> slots = new List<Command>();
 
+    public CommandDatabase commandDatabase;
 
+    void Start()
+    {
+        //Cache the database of commands so that we can always find any command we need.
+        commandDatabase = GameObject.FindGameObjectWithTag("CommandDatabase").GetComponent<CommandDatabase>();
+    }
 }
