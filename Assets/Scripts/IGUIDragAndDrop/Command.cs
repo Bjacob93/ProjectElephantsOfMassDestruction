fc257ee2efadd18c938bcd2ce03ffbac39546f8f
Runagar <runagar@gmail.com>
2016-10-20 11:29:28 +0200

    Added comments

--- Assets/Scripts/IGUIDragAndDrop/Command.cs
+++ Assets/Scripts/IGUIDragAndDrop/Command.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class Command : MonoBehaviour {
 
+    //Variables for all commands
     public string commandName;
     public int commandId;
     public string commandDesc;
 
+    //Empty constructor for a command
     public Command()
     {
 
     }
 
+    //Constructor for specific commands
     public Command (string name, int id, string desc)
     {
         commandName = name;
