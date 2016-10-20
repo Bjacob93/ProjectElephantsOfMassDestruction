1fa39af21bd8bb4217f6877664e2ec9f2b95da3f
Runagar <runagar@gmail.com>
2016-10-20 11:20:33 +0200

    Command class created, slot-scripts initialised

--- Assets/Scripts/IGUIDragAndDrop/Command.cs
+++ Assets/Scripts/IGUIDragAndDrop/Command.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Command : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public string commandName;
+    public int commandId;
+    public string commandDesc;
+
+    public Command()
+    {
+
+    }
+
+    public Command (string name, int id, string desc)
+    {
+        commandName = name;
+        commandId = id;
+        commandDesc = desc;
+    }
 }
