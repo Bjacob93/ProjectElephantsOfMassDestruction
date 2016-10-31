dd8bf808c91ee00c4ab9d0ace6856c666e1d3a5b
Bjacob93 <Zeint@live.dk>
2016-10-31 11:00:45 +0100

    limit enemy spawn
    it is now possible to limit the amount of enemies which can spawn

--- Assets/Scripts/Enemy_Scripts/BuyEnemyUnits.cs
+++ Assets/Scripts/Enemy_Scripts/BuyEnemyUnits.cs
@@ -5,7 +5,11 @@ public class BuyEnemyUnits : MonoBehaviour
 {
 
     public float restartDelay = 5f;         // Time to wait before restarting the level
-    public float restartTimer;                     // Timer to count up to restarting the level
+    public float restartTimer;              // Timer to count up to restarting the level
+
+    public int amountOfEnemySpawned = 0;    // Keep track of the amount of enemies spawn.
+    public int maxEnemySpawn = 20;          // Used to set the maximum amount of enemies a level can spawn. 
+                                            // This is used to define when the palyer wins eg when there are 0 enemies left the player wins
 
     public GameObject enemyUnits;
 
@@ -24,11 +28,15 @@ public class BuyEnemyUnits : MonoBehaviour
         }
 
         restartTimer += Time.deltaTime;
-        
+
         if (restartTimer >= restartDelay)
         {
-            Instantiate(enemyUnits, new Vector3(-45, 1, -45), Quaternion.Euler(0, 0, 0));
-            restartTimer = 0;
+            if (amountOfEnemySpawned < maxEnemySpawn)
+            {
+                Instantiate(enemyUnits, new Vector3(-45, 1, -45), Quaternion.Euler(0, 0, 0));
+                restartTimer = 0;
+                amountOfEnemySpawned++;
+            }
         }
     }
 
