1beea07c4056461d3a1c64db68775b3fb2a5c1b0
Bjacob93 <Zeint@live.dk>
2016-10-31 11:28:28 +0100

    hit chance + comments
    Added hit chance to the attack script and comments

--- Assets/Scripts/Enemy_Scripts/EnemyMelee_AI_Attack.cs
+++ Assets/Scripts/Enemy_Scripts/EnemyMelee_AI_Attack.cs
@@ -3,36 +3,52 @@ using System.Collections;
 
 public class EnemyMelee_AI_Attack : MonoBehaviour {
 
-	public float meleeCoolDown = 0.5f;
+	public float meleeCoolDown = 0.5f; //Attack cooldown
 	float meleeCoolDownLeft = 0f;
-	int attackDamage = 20; // damage of each attack
+	int attackDamage = 20; // Damage of each attack
 
-	public GameObject nearestPlayer;
-	public float MeleeRange = 3f;
+	public GameObject nearestPlayer; //Cache gameobject
+	public float MeleeRange = 3f;  // Melee range used to check if we can attack the enemy
 
     AstarEnemy aStarEnemy;
     AlliedMelee_AI_Health AiHealth;
 
+    float randV;        // float for a random value
+    float hitChance;   //float for the units hitChance
+
     void Start()
     {
+        //Find and store te AstarEnemy script
         aStarEnemy = this.GetComponent<AstarEnemy>();
     }
 	
 	void Update () {
 
-		nearestPlayer = aStarEnemy.nearestEnemy;
+        hitChance = 0.9f; // hit chance increase this to increase the hit chance eg 0.95 would be 95% hit chance instead of 90%
+        randV = Random.value; // calculate a random value used to determine if we hit the target
+
+        nearestPlayer = aStarEnemy.nearestEnemy; //Get the gameobject this script works with form the aStarEnemy script, this is the nearest enemy which is also the gameObject which is attacked
 
         if (nearestPlayer != null) {
+            //Calculate distance to enemy
             float dist = Vector3.Distance(this.transform.position, nearestPlayer.transform.position);
 
+            //Check if the enemy is in range and not null
             if (dist < MeleeRange && nearestPlayer != null)
             {
+                //Update the hit cooldown
                 meleeCoolDownLeft -= Time.deltaTime;
+                //Check if we need to run the attack script since we reached the targeted cooldown
                 if (meleeCoolDownLeft <= 0)
                 {
                     meleeCoolDownLeft = meleeCoolDown;
-
-                    nearestPlayer.GetComponent<AlliedMelee_AI_Health>().TakeDamage(attackDamage);
+                    //Create a random value between 0 and 1 if it is lower than 0.9 it hits, making the hit chance 90%
+                    if (randV < hitChance)
+                    {
+                        //Run the TakenDamage from AlliedMelee_AI_Health script to reduce the nearesPlayer health.
+                        nearestPlayer.GetComponent<AlliedMelee_AI_Health>().TakeDamage(attackDamage);
+                    }
+                    
                 }
             }
         }
