c7bea45fd42cd3962f138c7b8e625dfde1444d95
Bjacob93 <Zeint@live.dk>
2016-11-02 13:05:19 +0100

    commandDatabase

--- Assets/Scripts/IGUIDragAndDrop/CommandDatabase.cs
+++ Assets/Scripts/IGUIDragAndDrop/CommandDatabase.cs
@@ -23,21 +23,21 @@ public class CommandDatabase : MonoBehaviour {
         commandDatabase.Add (new Command ("Produce", "P01", "Produce a unit at base", false, false, true, new Vector3(),0));
 
         //For every order
-        commandDatabase.Add(new Command("For every ", "FoE", "The next line will run for every x amout of times for a unit, eg if the next line is attack checkpoint A and the one after it is B the units will switch between the two as the target they are going to", true, true, true, new Vector3(),0));
+        commandDatabase.Add(new Command("For every ", "FoE", "The next line will run for every x amout of times for a unit, eg if the next line is attack checkpoint A and the one after it is B the units will switch between the two as the target they are going to ", true, true, true, new Vector3(),0));
 
         //variable commands for foe?
-        commandDatabase.Add(new Command("second time", "FoE2", "this is a variable containing the number 2", false, true, true, new Vector3(), 2));
+        commandDatabase.Add(new Command("second time", "FoE2", "This var splits the units' target so that the first unit goes to A while the second unit goes to B ", false, true, true, new Vector3(), 2));
 
-        commandDatabase.Add(new Command("three times", "FoE3", "this is a variable containing the number 3", false, true, true, new Vector3(), 3));
+        commandDatabase.Add(new Command("three times", "FoE3", "This var splits the units' target so that the first and second unit goes to A while the third unit goes to B ", false, true, true, new Vector3(), 3));
 
         checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
         homebase = GameObject.FindGameObjectWithTag("PlayerBase");
         //scan for variables and automatically add them instead of doing it manually
         foreach (GameObject c in checkpoints)
         {
-            commandDatabase.Add(new Command("checkpoint " + c.name, "var" + c.name, "location of checkpoint " + c.name, false, true, true, c.transform.position,0));
+            commandDatabase.Add(new Command("checkpoint " + c.name, "var" + c.name, "This targets the location of the given checkpoint " + c.name, false, true, true, c.transform.position,0));
         }
 
-		commandDatabase.Add (new Command ("Homebase", "varPlayerBase", "location of Homebase", false, true, true, homebase.transform.position,0));   
+		commandDatabase.Add (new Command ("Homebase", "varPlayerBase", "This targets the location of the players base. ", false, true, true, homebase.transform.position,0));   
     }
 }
