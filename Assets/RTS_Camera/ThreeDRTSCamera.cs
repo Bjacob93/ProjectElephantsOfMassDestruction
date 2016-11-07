1427b65137909f9f0f183fe642f804b47b4e099e
Bjacob93 <Zeint@live.dk>
2016-11-07 10:23:53 +0100

    Camera starts zoomed out

--- Assets/RTS_Camera/ThreeDRTSCamera.cs
+++ Assets/RTS_Camera/ThreeDRTSCamera.cs
@@ -27,20 +27,21 @@ public class ThreeDRTSCamera : MonoBehaviour {
 	public float panAngleMin = 20;
 	public float panAngleMax = 85;
 
-	private Vector3 desiredPosition;
+    //Cameras Staring position
+	private Vector3 desiredPosition = new Vector3(0,30,0);
 
     Camera camera;
+    Vector3 move;
 
 	// Use this for initialization
 	void Start () {
         camera = GetComponent<Camera>();
-	}
+    }
 
 
 	// Update is called once per frame
 	void Update () {
 		//Camera movement
-
 		float x = 0, y = 0, z = 0;
 		float speed = scrollZone * 3 *  Time.deltaTime;
 		//WASD movement
@@ -71,20 +72,19 @@ public class ThreeDRTSCamera : MonoBehaviour {
 		//zoom camera
 		y -= 40 * Input.GetAxis ("Mouse ScrollWheel");
 
-		Vector3 move = new Vector3(x, y, z) + desiredPosition;
-		move.x = Mathf.Clamp (move.x, xMin, xMax);
-		move.y = Mathf.Clamp (move.y, 3, yMax);
+        move = new Vector3(x, y, z) + desiredPosition;
+        //limits the range the cameras movement can be eg The Camera cannot move beyond xMin,xMax etc.
+        move.x = Mathf.Clamp (move.x, xMin, xMax);
+		move.y = Mathf.Clamp (move.y, 5, yMax);
 		move.z = Mathf.Clamp (move.z, zMin, zMax);
 		desiredPosition = move;
-		transform.position = Vector3.Lerp(transform.position, desiredPosition, 0.2f);
-
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, 0.2f);
+ 
 		//panning when zooming not working
 		var pan = camera.transform.eulerAngles.x - (y/3) * -1;
 		pan = Mathf.Clamp (pan, panAngleMin, panAngleMax);
 		if (y < 0 || camera.transform.position.y > (yMax / 3)){
 			camera.transform.eulerAngles = new Vector3(pan,0,0);
 		}
-
-
 	}
 }
