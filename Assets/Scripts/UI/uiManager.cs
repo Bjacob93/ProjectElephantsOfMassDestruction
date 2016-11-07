d1acbb745648b103432f00364abd6d6687ec28c1
Runagar <runagar@gmail.com>
2016-11-07 14:09:49 +0100

    more UI stuff and ... meta files?

---
+++ Assets/Scripts/UI/uiManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uiManager : MonoBehaviour {
+
+    mainMenuVariables varKeeper;
+
+    GameObject uiStyle;
+
+
+	// Use this for initialization
+	void Start () {
+        varKeeper = GameObject.Find("KeeperOfVariables").GetComponent<mainMenuVariables>();
+
+        if (varKeeper.useDragonDrop)
+        {
+            uiStyle = GameObject.Find("TextEditing");
+            uiStyle.SetActive(false);
+        }else
+        {
+            uiStyle = GameObject.Find("DragonDrop");
+            uiStyle.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
