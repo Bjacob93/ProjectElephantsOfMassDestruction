8724192c7d6968dd0f79ec7fb7c34587f19bc893
Yoh874 <micuks1@gmail.com>
2016-11-09 11:11:55 +0100

    helmet

---
+++ Assets/Scripts/Equip Weapon/helmetHolder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class helmetHolder : MonoBehaviour {
+
+	public Transform Head;
+	public helmet startinghelmet;
+	helmet equippedHelmet;
+
+	void Start(){
+		if (startinghelmet != null) {
+			equipHelmet(startinghelmet);
+		}
+	}
+	public void equipHelmet(helmet helmetToEquip){
+		if (equippedHelmet != null) {
+			Destroy (equippedHelmet.gameObject);
+		}
+		equippedHelmet = Instantiate(helmetToEquip, Head.position, Head.rotation) as helmet;
+		equippedHelmet.transform.parent = Head;
+	}
+}
