9829cf3e7476afcad06c53236d88eab5f583941e
Bjacob93 <Zeint@live.dk>
2016-11-14 15:21:55 +0100

    allied atk fix

--- Assets/Scripts/Allied_Scripts/AlledMelee_AI_Attack.cs
+++ Assets/Scripts/Allied_Scripts/AlledMelee_AI_Attack.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AlledMelee_AI_Attack : MonoBehaviour {
+public class AlledMelee_AI_Attack : MonoBehaviour
+{
 
-	public Animator anim;
+    public Animator anim;
 
-	public float meleeCoolDown = 1.11f; // attack cooldown
-	float meleeCoolDownLeft = 0f;
-	float attackDamage; // damage of each attack
+    public float meleeCoolDown = 1.11f; // attack cooldown
+    float meleeCoolDownLeft = 0f;
+    float attackDamage; // damage of each attack
 
-	public GameObject nearestPlayer; //Cache gameobject
+    public GameObject nearestPlayer; //Cache gameobject
     public float MeleeRange = 3f;   // Melee range used to check if we can attack the enemy
 
     float randV; // float for a random value
@@ -18,16 +19,17 @@ public class AlledMelee_AI_Attack : MonoBehaviour {
 
     Astar aStar;
 
-    void Start ()
+    void Start()
     {
-		anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
 
         aStar = this.GetComponent<Astar>();
 
         eachMissIncreaseChance = 0;
     }
 
-    void Update() {
+    void Update()
+    {
         //attackDamage = Random.Range(10f, 20f);
         //hitChance = 0.9f; // hit chance increase this to increase the hit chance eg 0.95 would be 95% hit chance instead of 90%
         //randV = Random.value;   // calculate a random value used to determine if we hit the target
@@ -69,35 +71,41 @@ public class AlledMelee_AI_Attack : MonoBehaviour {
         //}
     }
 
-    public void alliesAttack(GameObject nearestPlayer) {
+    public void alliesAttack(GameObject nearestPlayer)
+    {
         attackDamage = Random.Range(15f, 20f);
         hitChance = 0.9f; // hit chance increase this to increase the hit chance eg 0.95 would be 95% hit chance instead of 90%
         randV = Random.value;   // calculate a random value used to determine if we hit the target
 
-        if (nearestPlayer != null){
-                //Update the hit cooldown
-                meleeCoolDownLeft -= Time.deltaTime;
-                //Check if we need to run the attack script since we reached the targeted cooldown
+        if (nearestPlayer != null)
+        {
+            //Update the hit cooldown
+            meleeCoolDownLeft -= Time.deltaTime;
+            //Check if we need to run the attack script since we reached the targeted cooldown
+            if (meleeCoolDownLeft <= 0.5f)
+            {
+                if (!this.anim.GetCurrentAnimatorStateInfo(0).IsName("ATTACK"))
+                {
+                    anim.Play("ATTACK", -1, 0f);
+                }
                 if (meleeCoolDownLeft <= 0)
                 {
-					if (!this.anim.GetCurrentAnimatorStateInfo(0).IsName("ATTACK"))
-					{
-						anim.Play("ATTACK", -1, 0f);
-					}  
-                    meleeCoolDownLeft = meleeCoolDown;
-                
+
                     //find check if the attack connects/hits, atm there are 90% for hit
-                    if(randV < hitChance + eachMissIncreaseChance)
+                    if (randV < hitChance + eachMissIncreaseChance)
                     {
                         //Run the TakenDamage from AlliedMelee_AI_Health script to reduce the nearesPlayer health.
                         nearestPlayer.GetComponent<EnemyMelee_AI_Health>().TakeDamage(attackDamage);
                         eachMissIncreaseChance = 0;
+                        meleeCoolDownLeft = meleeCoolDown;
                     }
                     else
                     {
                         eachMissIncreaseChance += 5;
+                        meleeCoolDownLeft = meleeCoolDown;
                     }
                 }
+            }
         }
-	}
+    }
 }
