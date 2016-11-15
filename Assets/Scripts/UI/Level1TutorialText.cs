eff7fdf51aeea7ca01e21912ed21bf074f86f911
Bjacob93 <Zeint@live.dk>
2016-11-15 15:06:37 +0100

    drag and drop tutorial text working with background

--- Assets/Scripts/UI/Level1TutorialText.cs
+++ Assets/Scripts/UI/Level1TutorialText.cs
@@ -30,12 +30,12 @@ public class Level1TutorialText : MonoBehaviour {
             TutorialPage8,
             TutorialPage9;
 
-    string dndTutorialPage1 = "Lets begin! \n " +
-                                "The Elephants attacks the homebase and we need to defend it, our goal is to eliminate all of the Elephants and Capture the flag \n " +
+    string dndTutorialPage1 =   "Lets begin! \n" +
+                                "The Elephants attacks the homebase and we need to defend it, our goal is to eliminate all of the Elephants and Capture the flag \n" +
                                 "In order to move around the map, you can use your mouse or WASD-buttons on the keyboard. \n  \n" +
                                 "Press Tab-button on the keyboard or the show button on the screen to show the Command list.",
 
-            dndTutorialPage2 = "This is the Command List, here are all of the commands which areavailable in the current level. To read more about the commandshover your curser over each command to read a description. \n You can close the Command list at any time by pressing tab or hide \n \n " +
+            dndTutorialPage2 = "This is the Command List, here are all of the commands which areavailable in the current level. To read more about the commandshover your curser over each command to read a description. \n You can close the Command list at any time by pressing tab or hide \n \n" +
                                 "Press the castle to open up the sequence editor",
 
             dndTutorialPage3 = "This is the editor here you place the commands from the command list. \n" +
@@ -45,24 +45,24 @@ public class Level1TutorialText : MonoBehaviour {
             dndTutorialPage4 = "The produce order creates giraffes at the player’s base, these giraffes receive the commands from the flag and homebase editors.\n \n" +
                                 "Drag the “Attack” order onto the next line to continue",
 
-            dndTutorialPage5 = "The attack command orders your giraffes at the editor's location to attack a set destination which is placed to the right of the attack order.\n \n " +
+            dndTutorialPage5 = "The attack command orders your giraffes at the editor's location to attack a set destination which is placed to the right of the attack order.\n \n" +
                                 "Now give the Attack command the checkpoint A",
 
             dndTutorialPage6 = "The target checkpoint, in this case A is the place on the map where the “A” flag is placed. There can be multiple flags on a map, some of which have a capture point. The player needs to capture and hold these points in order to win. \n \n" +
                                "Now close the editor and scroll the camera left until you see a flag on the ground. click it to open its editor.",
 
-            dndTutorialPage7 = "This is the editor window for checkpoint A. Have you noticed that the commands you entered at the castle are not present here? This is because each location on the map has its own editor. \n \n " +
+            dndTutorialPage7 = "This is the editor window for checkpoint A. Have you noticed that the commands you entered at the castle are not present here? This is because each location on the map has its own editor. \n \n" +
                                 "Drag a defend command into the editor.",
         
             dndTutorialPage8 = "The Defend command will order any of your units that gets near the point to defend it. It also gives them a slight bonus to their armour. \n" + "Now you are ready to defend against the onslought of the savage elephants, go forth and be victorious  \n \n" +
                                 "Press play to begin";
 
-    string  textTutorialPage1 = "Lets begin! \n " +
-                                "The Elephants attacks the homebase and we need to defend it, our goal is to eliminate all of the Elephants and Capture the flag \n " +
+    string  textTutorialPage1 = "Lets begin! \n" +
+                                "The Elephants attacks the homebase and we need to defend it, our goal is to eliminate all of the Elephants and Capture the flag \n" +
                                 "In order to move around the map, you can use your mouse or WASD-buttons on the keyboard. \n  \n" +
                                 "Press Tab-button on the keyboard or the show button on the screen to show the Command list.",
 
-            textTutorialPage2 = "This is the Command List, here are all of the commands which areavailable in the current level. To read more about the commandshover your curser over each command to read a description. \n \n " +
+            textTutorialPage2 = "This is the Command List, here are all of the commands which areavailable in the current level. To read more about the commandshover your curser over each command to read a description. \n \n" +
                                 "Press the castle to open up the sequence editor",
 
             textTutorialPage3 = "",
@@ -82,7 +82,7 @@ public class Level1TutorialText : MonoBehaviour {
     {
         ToturialBoxStartPosX = (Screen.width / 2) - (Screen.width / 6);
         ToturialBoxStartPosY = (Screen.height / 2) + (Screen.height / 6);
-        TutorialBoxHeight = Screen.height / 6;
+        TutorialBoxHeight = Screen.height / 4;
         TutorialBoxWidth = Screen.width / 3;
         drawStartInfo = true;
         TutorialBox = new Rect(ToturialBoxStartPosX, ToturialBoxStartPosY, TutorialBoxWidth, TutorialBoxHeight);
@@ -125,40 +125,40 @@ public class Level1TutorialText : MonoBehaviour {
        { 
             if (qHasBeenPressed == false/* && editorHasBeenOpened == false && enterProduceOrder == false && enterAttackOrder == false && enterAttackTarget == false && pressPlay == false*/)
             {
-                GUI.Box(new Rect(TutorialBox), TutorialPage1, commandSkin.GetStyle("tooltipBackground"));
+                GUI.Box(new Rect(TutorialBox), TutorialPage1, commandSkin.GetStyle("tutorialBoundingBoxBackground"));
             }
             if (qHasBeenPressed == true && editorHasBeenOpened == false /*&& enterProduceOrder == false && enterAttackOrder == false && enterAttackTarget == false && pressPlay == false*/)
             {
-                GUI.Box(new Rect(TutorialBox), TutorialPage2, commandSkin.GetStyle("tooltipBackground"));
+                GUI.Box(new Rect(TutorialBox), TutorialPage2, commandSkin.GetStyle("tutorialBoundingBoxBackground"));
             }
 
             if (qHasBeenPressed == true && editorHasBeenOpened == true && enterProduceOrder == false/* && enterAttackOrder == false && enterAttackTarget == false && pressPlay == false*/)
             {
-                GUI.Box(new Rect(TutorialBox), TutorialPage3, commandSkin.GetStyle("tooltipBackground"));
+                GUI.Box(new Rect(TutorialBox), TutorialPage3, commandSkin.GetStyle("tutorialBoundingBoxBackground"));
             }
 
             if (qHasBeenPressed == true && editorHasBeenOpened == true && enterProduceOrder == true && enterAttackOrder == false /*&& enterAttackTarget == false && pressPlay == false*/)
             {
-                GUI.Box(new Rect(TutorialBox), TutorialPage4, commandSkin.GetStyle("tooltipBackground"));
+                GUI.Box(new Rect(TutorialBox), TutorialPage4, commandSkin.GetStyle("tutorialBoundingBoxBackground"));
             }
 
             if (qHasBeenPressed == true && editorHasBeenOpened == true && enterProduceOrder == true && enterAttackOrder == true && enterAttackTarget == false/* && pressPlay == false*/)
             {
-                GUI.Box(new Rect(TutorialBox), TutorialPage5, commandSkin.GetStyle("tooltipBackground"));
+                GUI.Box(new Rect(TutorialBox), TutorialPage5, commandSkin.GetStyle("tutorialBoundingBoxBackground"));
             }
 
             if (qHasBeenPressed == true && editorHasBeenOpened == true && enterProduceOrder == true && enterAttackOrder == true && enterAttackTarget == true && chechpointEditorOpened == false)
             {
-                GUI.Box(new Rect(TutorialBox), TutorialPage6, commandSkin.GetStyle("tooltipBackground"));
+                GUI.Box(new Rect(TutorialBox), TutorialPage6, commandSkin.GetStyle("tutorialBoundingBoxBackground"));
             }
 
             if (qHasBeenPressed == true && editorHasBeenOpened == true && enterProduceOrder == true && enterAttackOrder == true && enterAttackTarget == true && chechpointEditorOpened == true && defendOrderInserted == false)
             {
-                GUI.Box(new Rect(TutorialBox), TutorialPage7, commandSkin.GetStyle("tooltipBackground"));
+                GUI.Box(new Rect(TutorialBox), TutorialPage7, commandSkin.GetStyle("tutorialBoundingBoxBackground"));
             }
             if (qHasBeenPressed == true && editorHasBeenOpened == true && enterProduceOrder == true && enterAttackOrder == true && enterAttackTarget == true && chechpointEditorOpened == true && defendOrderInserted == true && pressPlay == false)
             {
-                GUI.Box(new Rect(TutorialBox), TutorialPage8, commandSkin.GetStyle("tooltipBackground"));
+                GUI.Box(new Rect(TutorialBox), TutorialPage8, commandSkin.GetStyle("tutorialBoundingBoxBackground"));
             }
             if (qHasBeenPressed == true && editorHasBeenOpened == true && enterProduceOrder == true && enterAttackOrder == true && enterAttackTarget == true && chechpointEditorOpened == true && defendOrderInserted == true && pressPlay == true)
             {
